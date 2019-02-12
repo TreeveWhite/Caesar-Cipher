b5fd19bfaa18eb706d7d75b3b69b5e5ca7eab4f2
TWhite <47389365+TreeveWhite@users.noreply.github.com>
2019-02-12 09:54:00 +0000

    Adjustments to dictionary search and GUI.
    Changed the layout of the UI to be more user friendly and changed the search algorithm in the brute decrypt function to increase accuracy.

--- Ceasar Cipher.py
+++ Ceasar Cipher.py
@@ -16,9 +16,9 @@ def encrypt_caesar(plain_text, shift):
         stayInAlphabet += 26
     finalLetter = chr(stayInAlphabet)
     cipherText += finalLetter
-  application.changeAwnserLbl("The Encyption is: " + cipherText)
+  return cipherText
 
-def decrypt_caesar(plain_text, shift, i):
+def decrypt_caesar(plain_text, shift):
   cipherText = ''
   for ch in plain_text:
     stayInAlphabet = ord(ch) + shift
@@ -36,52 +36,77 @@ def decrypt_caesar(plain_text, shift, i):
     cipherText += finalLetter
   
   #Find if the decrypted word is in the dictionary.txt
-  key = cipherText.lower()
-  import mmap
-  info = False
-  bytes = str.encode(str(key.lower()))
-  type(bytes)
-
-  with open('dictionary.txt', 'rb', 0) as file, \
-    mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
-    if s.find(bytes) != -1:
-        info = True
-
+  print(cipherText)
   with open("dictionary.txt", "r+") as file:
-    if info == True:
-      for line in file:
-        if key.lower() in line:
-          application.changeAwnserLbl("The Transcript is: " + key)
-          break
+    for line in file:
+      if cipherText.lower() == line.strip():
+        print(cipherText, line.strip())
+        return cipherText
+        break
+      
 
 def startencrypt():
-  application.changeAwnserLbl("")
   message = application.entry.get()
   shift = random.randint(0, 26)
-  encrypt_caesar(message, shift)
+  application.changeAwnserLbl("Encyrpted Message: {}".format(encrypt_caesar(message, shift)))
 
 def bruteforce():
+  decryptMessage = ""
   application.changeAwnserLbl("")
   message = application.entry.get()
-  for i in range(26):
-      shift = i
-      decrypt_caesar(message, shift, i)
+  for shift in range(26):
+    decryptWord = decrypt_caesar(message, shift)
+    if decryptWord != None:
+      break
+  application.changeAwnserLbl("Decrypted Message: {}".format(decryptWord))
 
 #---------------------------GUI
 class ui:
   def __init__(self, master):
     self.master = master
     
-    self.title = tkinter.Label(self.master, text="Caesar Cipher", font=("Algerian", "20"), fg="black", bg="azure4").place(x=30, y=0)
+    self.title = tkinter.Label(self.master,
+                               text="Caesar Cipher",
+                               font=("Algerian", "20"),
+                               fg="black",
+                               bg="azure4"
+                               ).place(x=45, y=0)
 
-    self.txt = tkinter.Label(self.master, text="Text: ", font=("Algerian", "12"), bg="azure4").place(x=0, y=50)
-    self.entry = tkinter.Entry(self.master, font=("Arial", "12"), bg="DarkGoldenrod1")
-    self.entry.place(x=50, y=50)
+    self.txt = tkinter.Label(self.master,
+                             text="Text: ",
+                             font=("Algerian", "12"),
+                             bg="azure4"
+                             ).place(x=38, y=50)
+    
+    self.entry = tkinter.Entry(self.master,
+                               font=("Adobe Garamond Pro Bold", "12"),
+                               bg="DarkGoldenrod1")
+    self.entry.place(x=88, y=50)
 
-    self.brute = tkinter.Button(self.master, text="Brute Force\nDecrypt", font=("Arial", "12"), command=bruteforce, bg="DarkGoldenrod1").place(x=45, y=100)
-    self.encrypt = tkinter.Button(self.master, text="Encrypt\n", font=("Arial", "12"), command=startencrypt, bg="DarkGoldenrod1").place(x=170, y=100)
-    self.awnser = tkinter.Label(self.master, text="", font=("Arial", "12"), bg="azure4")
-    self.awnser.place(x=50, y=75)
+    self.brute = tkinter.Button(self.master,
+                                text="Brute Force\nDecrypt",
+                                font=("Adobe Garamond Pro Bold", "12"),
+                                command=bruteforce,
+                                bg="DarkGoldenrod1",
+                                width=11
+                                ).place(x=35, y=100)
+    
+    self.encrypt = tkinter.Button(self.master,
+                                  text="Encrypt\n",
+                                  font=("Adobe Garamond Pro Bold", "12"),
+                                  command=startencrypt,
+                                  bg="DarkGoldenrod1",
+                                  width=11
+                                  ).place(x=160, y=100)
+    
+    self.awnser = tkinter.Label(self.master,
+                                text="",
+                                width=28,
+                                height=5,
+                                bg="DarkGoldenrod1",
+                                relief = "groove",
+                                font=("Adobe Garamond Pro Bold", "12"))
+    self.awnser.place(x=21, y=175)
 
   def changeAwnserLbl(self, text):
     self.awnser.configure(text=text)
@@ -90,9 +115,9 @@ class ui:
 if __name__ == "__main__":
   root = tkinter.Tk()
   root.title("Caesar Cipher")
-  root.geometry("250x200")
+  root.geometry("300x300")
   root.configure(bg="azure4")
-
+  root.resizable(False, False)
   application = ui(root)
 
   root.mainloop()
